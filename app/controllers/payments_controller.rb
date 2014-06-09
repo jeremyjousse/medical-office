7a79246588ab810f13222609f05323b465558b31
Jérémy Jousse <jeremy.jousse@coevo.fr>
2014-06-09 21:41:33 +0200

    Fix payment listing

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -39,7 +39,7 @@ class PaymentsController < ApplicationController
 
     @q = current_user.payments.paginate(:page => search_params[:page], :per_page => search_params[:per_page]).search(search_params[:search])
     @payments = @q.result(distinct: true).order(:paid_at, :payment_type)
-    @total_items = current_user.payments.find(:all).count
+    @total_items = current_user.payments.all().count
     @total_items_selected = @payments.count
 
   end
