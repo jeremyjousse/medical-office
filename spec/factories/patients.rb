75429f3f7e611883abf4b6248a6ee5eba13e62ed
jeremyjousse <jeremy.jousse@coevo.fr>
2016-02-24 20:11:46 +0100

    Fix Patient factories to pass model spec

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :patient do
     first_name 'Sidney'
     last_name 'Johns'
+    city 'Lille'
+    country_id 1
     created_at Time.now
     updated_at Time.now
   end
