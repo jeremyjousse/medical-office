88b526fd3895dce2eacd75e07c2fa72152af7eaf
Jérémy Jousse <jeremy.jousse@gmail.com>
2021-01-26 21:07:18 +0100

    feat(payments): add treatment type filter

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,50 +1,44 @@
-class PaymentsController < ApplicationController
+# frozen_string_literal: true
 
+# PaymentsController is dispaying payments list and
+# payments listing in order to be printed
+class PaymentsController < ApplicationController
   before_filter :authenticate_user!
 
-  before_action :set_payment, only: [:show, :edit, :update, :destroy]
+  before_action :set_payment, only: %i[show edit update destroy]
 
   include ListingHelper
 
   def index
-    if params[:per_page].nil?
-      params[:per_page] = 10
-    end
-    @search_params = put_and_get_search_params_in_session('payments',{'search' => params[:q], 'page' => params[:page], 'per_page' => params[:per_page]},params[:filter])
+    params[:per_page] = 10 if params[:per_page].nil?
+    @search_params = put_and_get_search_params_in_session('payments', { 'search' => params[:q], 'page' => params[:page], 'per_page' => params[:per_page] }, params[:filter])
 
-
-    @q = current_user.payments.order(paid_at: :desc).paginate(:page => @search_params['page'], :per_page => @search_params['per_page']).search(@search_params['search'])
+    @q = current_user.payments.order(paid_at: :desc).paginate(page: @search_params['page'], per_page: @search_params['per_page']).search(@search_params['search'])
     @payments = @q.result(distinct: true)
-    @total_items = current_user.payments.all().count
+    @total_items = current_user.payments.all.count
     @total_items_selected = @payments.count
-
-
   end
 
   def listing
-    if params[:per_page].nil?
-      params[:per_page] = 1000
-    end
-    @search_params = put_and_get_search_params_in_session('listing_payments',{'search' => params[:q], 'page' => params[:page], 'per_page' => params[:per_page]},params[:filter])
+    params[:per_page] = 1000 if params[:per_page].nil?
+
+    @search_params = put_and_get_search_params_in_session('listing_payments', { 'search' => params[:q], 'page' => params[:page], 'per_page' => params[:per_page] }, params[:filter])
 
     @q = current_user.payments.includes(:medical_treatment, medical_treatment: :patient, medical_treatment: :medical_treatment_type).search(@search_params['search'])
-    @payments = current_user.payments.includes(medical_treatment: [:patient, :medical_treatment_type]).paginate(:page => @search_params['page'], :per_page => @search_params['per_page']).search(@search_params['search']).result(distinct: true).order(:paid_at, :payment_type)
-    @total_items = current_user.payments.all().count
-    
+    @payments = current_user.payments.includes(medical_treatment: %i[patient medical_treatment_type]).paginate(page: @search_params['page'], per_page: @search_params['per_page']).search(@search_params['search']).result(distinct: true).order(:paid_at, :payment_type)
+    @total_items = current_user.payments.all.count
     @total_items_selected = @payments.count
-
   end
 
   # GET /payments/1
   # GET /payments/1.json
-  def show
-  end
+  def show; end
 
   # GET /payments/new
   def new
     @payment = Payment.new
 
-    if !params[:medical_treatment_id].nil? then
+    unless params[:medical_treatment_id].nil?
       @payment.medical_treatment_id = params[:medical_treatment_id]
 
       @medical_treatment = MedicalTreatment.find(params[:medical_treatment_id])
@@ -52,23 +46,18 @@ class PaymentsController < ApplicationController
       @payment.amount = @medical_treatment.price
     end
 
-    if !params[:payment_type].nil? then
-      @payment.payment_type = params[:payment_type].to_i
-    end
+    @payment.payment_type = params[:payment_type].to_i unless params[:payment_type].nil?
 
-    if @payment.payment_type.to_i == 1 then
+    if @payment.payment_type.to_i == 1
       @payment.build_payment_bank_check
       @payment.payment_bank_check.account_owner = @medical_treatment.patient.last_name
     end
 
     @payment.paid_at = Date.today
-
   end
-
 
   # GET /payments/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /payments
   # POST /payments.json
@@ -77,7 +66,7 @@ class PaymentsController < ApplicationController
 
     @payment.user = current_user
 
-    if !@payment.payment_bank_check.nil? then
+    unless @payment.payment_bank_check.nil?
       @payment.payment_bank_check.user = current_user
       @payment.payment_bank_check.amount = @payment.amount
       @payment.payment_bank_check.status = 0
@@ -87,12 +76,12 @@ class PaymentsController < ApplicationController
       if @payment.save
         format.html { redirect_to @payment, notice: 'Payment was successfully created.' }
         format.json { render action: 'show', status: :created, location: @payment }
-        #format.js { redirect_to @payment.medical_treatment, notice: 'Payment was successfully created.' }
+        # format.js { redirect_to @payment.medical_treatment, notice: 'Payment was successfully created.' }
         format.js { render 'reload' }
       else
         format.html { render action: 'new' }
         format.json { render json: @payment.errors, status: :unprocessable_entity }
-        format.js { render 'new', {medical_treatment_id: params[:medical_treatment_id], payment_type: params[:payment_payment_type]}}
+        format.js { render 'new', { medical_treatment_id: params[:medical_treatment_id], payment_type: params[:payment_payment_type] } }
       end
     end
   end
@@ -114,14 +103,9 @@ class PaymentsController < ApplicationController
   # DELETE /payments/1
   # DELETE /payments/1.json
   def destroy
-
     destroyed = false
 
-    if @payment.safe_destroy == true
-        destroyed = true
-    end
-
-
+    destroyed = true if @payment.safe_destroy == true
 
     respond_to do |format|
       format.html { redirect_to payments_url }
@@ -130,16 +114,14 @@ class PaymentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_payment
-      @payment = Payment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def payment_params
-      params.require(:payment).permit(:medical_treatment_id, :paid_at, :payment_type, :amount, payment_bank_check_attributes: [:id, :bank_name, :account_owner, :check_number])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_payment
+    @payment = Payment.find(params[:id])
+  end
 
-
-
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def payment_params
+    params.require(:payment).permit(:medical_treatment_id, :paid_at, :payment_type, :amount, payment_bank_check_attributes: %i[id bank_name account_owner check_number])
+  end
 end
